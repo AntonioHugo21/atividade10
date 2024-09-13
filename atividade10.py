3de61e0e681b7db6d851c0f887e3620a51119c51
AntonioHugo21 <antoniohugo.pierre@gmail.com>
2024-09-13 11:44:13 -0300

    numeros pares e ímpares

---
+++ atividade10.py
@@ -0,0 +1,9 @@
+# Crie um programa que receba um número inteiro e informe se ele é par ou ímpar.
+
+num_inteiro = int(input("digite o numero 1"))
+
+if (num_inteiro % 2 == 0):
+    print("o num_inteiro é par")
+else:
+    print("o num_inteiro é ímpar")
+    
